b5151c4d55310e5693a3bccb5d985d8b7984d572
pvtHenk <jannesdrijkoningen@gmail.com>
2018-02-16 17:20:20 +0100

    More operator overloads

--- Sandbox2D/Structs.h
+++ Sandbox2D/Structs.h
@@ -9,13 +9,13 @@ struct RGBA
 };
 
 //! Creates an RGBA struct using alpha
-inline RGBA MakeRGBA(Uint8 red, Uint8 green, Uint8 blue, Uint8 alpha)
+inline RGBA MakeRGBA(const Uint8 red, const Uint8 green, const Uint8 blue, const Uint8 alpha)
 {
 	return RGBA{ red, green, blue, alpha };
 }
 
 //! Creates an RGBA struct with alpha = 255
-inline RGBA MakeRGBA(Uint8 red, Uint8 green, Uint8 blue)
+inline RGBA MakeRGBA(const Uint8 red, const Uint8 green, const Uint8 blue)
 {
 	return RGBA{ red, green, blue, 255 };
 }
